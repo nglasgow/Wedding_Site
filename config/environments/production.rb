ce776b9e3e259a2becca09ca81e5b3281feecdc7
Nicole Glasgow <nicole.glasgow@ymail.com>
2014-07-22 17:31:06 -0700

    update postback2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ WeddingSite::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'www.gabeandnicole.com'}
+  config.action_mailer.default_url_options = { :host => 'mighty-basin-4255.heroku.com'}
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :postmark
