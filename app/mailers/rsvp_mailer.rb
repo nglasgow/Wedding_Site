21830f9a965ee9d0553ae20f08dab5fe33c09ee8
Nicole Glasgow <nicole.glasgow@ymail.com>
2014-07-24 20:45:20 -0700

    updatemail3

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -7,6 +7,6 @@ class RsvpMailer < ActionMailer::Base
 
     subject_prefix = primary.attending? ? "Attending" : "Not Attending"
 
-    mail(:from => '"www.gabeandnicole.com" <no-reply@gabeandnicole.com>', :to => 'rsvp.rsvp_email.deliver' :subject => "New RSVP - #{subject_prefix} : #{primary.first_name} #{primary.last_name} + #{@guests.size}")
+    mail(:from => '"www.gabeandnicole.com" <no-reply@gabeandnicole.com>', :to => 'rsvp_email.deliver' :subject => "New RSVP - #{subject_prefix} : #{primary.first_name} #{primary.last_name} + #{@guests.size}")
   end
 end
