dfcedd8f8a0e4011ff55ef32e38733db5944e340
Nicole Glasgow <nicole.glasgow@ymail.com>
2014-08-03 17:31:15 -0700

    mailer2

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,7 +4,7 @@ class ContactMailer < ActionMailer::Base
   def inbound_email(email, subject, message)
     @subject = subject
     @message  = message
-    mail(:from => email , :to => 'fcf1e0511c55efa2ee3e8b4bdf0a0dd8@inbound.postmarkapp.com', :subject => "Message from www.gabeandnicole.com: #{subject}")
+    mail(:from => email, :to => 'fcf1e0511c55efa2ee3e8b4bdf0a0dd8@inbound.postmarkapp.com', :cc => 'gabe.nicole.wed@gmail.com', :subject => "Message from www.gabeandnicole.com: #{subject}")
   end
 
 end
